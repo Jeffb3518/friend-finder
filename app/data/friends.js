85ef0a80669c4748e1ef0b8ed466572b33c62c64
Jeffrey Bauer <Jeffrey@Jeffreys-Air.attlocal.net>
2017-01-13 11:46:03 -0500

    updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,17 @@
-var users = [{
-    name: "Jeff",
-    scores: [1,2,3,4,5,6,7,8,9,10]
-}];
+var users = {
+    "name": "Jeff",
+    "scores": [
+        5,
+        6,
+        7,
+        8,
+        9,
+        1,
+        2,
+        3,
+        4,
+        10
+    ]
+};
 
 module.exports = users;
